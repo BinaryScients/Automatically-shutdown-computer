a63d6ed3dbd352de9c4f5b7823ab4a463da7433b
BinaryScients <willianazevedo974@gmail.com>
2020-10-11 13:24:30 -0300

    Creating the main file

---
+++ Sleeping aid.bat
@@ -0,0 +1,7 @@
+:: Author: Willian Azevedo 
+:: Created on: 11th, October, 2020 at 01:15 P.M. (GMT-3) 
+:: Last Modification on: 11th, October, 2020 at 01:15 P.M. (GMT-3) 
+:: Sleeping Aid - Automatic shutdown porgram version 0.1.0 Alpha 
+:: 
+:: This is a simple batch program for automaticly shutdown pc 
+ 
