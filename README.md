26529e522ac69963258e1e1280046a73b14d9844
BinaryScients <willianazevedo974@gmail.com>
2020-10-14 01:50:47 -0300

    Adding the restart funcion, the delete function and fixing some typos

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This is a simple program that helps windows users to program their computers to shutdown 
- 
-With a simple command + enter you can program your computer to shutdown at the time that you want 
-Otherwise you can setup it to shutdown everyday at the time that you have configured before 
+Automatically Shutdown Computer - Sleeping Aid program verion 0.1.5 Alpha
+
+This is a simple program that helps Windows users to program their computers to shutdown 
+
+With some enters you can schedule your computer to shutdown everyday at the time that you want.
+Besides that, you can schedule the pc to restart...
--- Sleeping aid.bat
+++ Sleeping aid.bat
@@ -1,13 +1,18 @@
 :: Author: Willian Azevedo 
 :: Created on: 11th, October, 2020 at 01:15 P.M. (GMT-3) 
-:: Last Modification on: 11th, October, 2020 at 01:15 P.M. (GMT-3) 
-:: Sleeping Aid - Automatic shutdown program version 0.1.0 Alpha 
+:: Last Modification on: 14th, October, 2020 at 12:12 A.M. (GMT-3) 
+:: Sleeping Aid - Automatic shutdown program version 0.1.5 Alpha 
 :: 
-:: This is a simple batch program for automaticly shutdown pc
+:: This is a simple batch program for automatically shutdown pc
 
 ::TODO
-:: Implement an option that verify if already has a task schedule
+:: Implement an option that verify if already has a task schedule [-] (in progress)
 :: Fix somethings
+
+:: Fix the color theme choice menu problems
+:: Fix the menus of the pc shutdown
+
+
 
 set themec=0a
 :load
@@ -16,12 +21,12 @@ set themec=0a
 :start
 @echo off
 color %themec%
-title Sleeping aid - Automatic shutdown program (v0.1.0 alpha)
+title Sleeping aid - Automatic shutdown program (v0.1.5 alpha)
 cls
 
 :main
 echo Sleeping Aid - Automatic Shutdown Program for Windows
-echo version 0.1.0 Alpha
+echo version 0.1.5 Alpha
 echo.
 echo        _    _  ____  __    ___  _____  __  __  ____ 
 echo       ( \/\/ )( ___)(  )  / __)(  _  )(  \/  )( ___)
@@ -77,7 +82,7 @@ cls&goto finish
 
 :: schtasks /create /sc daily /tn PCSHUTDOWN /tr cmd shutdown /s /f /c "The windows will finish" /st 22:30 /sd 03/01/2020
 :: schtasks /create /sc <scheduletype> /tn <taskname> /tr <taskrun> [/s <computer> [/u [<domain>\]<user> [/p <password>]]] [/ru {[<domain>\]<user> | system}] [/rp <password>] [/mo <modifier>] [/d <day>[,<day>...] | *] [/m <month>[,<month>...]] [/i <idletime>] [/st <starttime>] [/ri <interval>] [{/et <endtime> | /du <duration>} [/k]] [/sd <startdate>] [/ed <enddate>] [/it] [/z] [/f]
-:: acess this: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/schtasks-create
+:: see this: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/schtasks-create
 
 :schedule1
 echo Schedule the pc to shutdown
@@ -90,7 +95,7 @@ if errorlevel 1 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 echo.
 set /p dt="Enter the date (DD/MM/YYYY) you want this rule to start taking effect (by default it's considered today, type today for default option): "
@@ -101,7 +106,7 @@ if errorlevel 1 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 if "%dt%" == "today" set dt=%DATE%
 echo What frequency do you want it to turn off?
@@ -122,7 +127,7 @@ if errorlevel 1 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if "%frec%" == "1" set frec=ONCE
@@ -139,7 +144,7 @@ if errorlevel 1 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if errorlevel 2 (
@@ -149,7 +154,7 @@ if errorlevel 2 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if errorlevel 3 (
@@ -159,7 +164,7 @@ if errorlevel 3 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if errorlevel 4 (
@@ -169,7 +174,7 @@ if errorlevel 4 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if errorlevel 5 (
@@ -179,7 +184,7 @@ if errorlevel 5 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 
 if errorlevel 6 (
@@ -189,7 +194,7 @@ if errorlevel 6 (
 	echo        [PRESS ENTER TYPE AGAIN]
 	pause>nul
 	color %themec%
-	cls&goto schedule
+	cls&goto schedule1
 )
 echo.
 echo Applying settings...
@@ -207,7 +212,7 @@ if errorlevel 1 (
 	cls&goto finish
 )
 echo.
-echo SUCESS: The computer has been programmed to shut down on %dt% at %tm% successfully
+echo SUCCESS: The computer has been programmed to shut down on %dt% at %tm% successfully
 echo.
 echo         [PRESS ENTER GO TO MENU]
 echo.
@@ -215,8 +220,196 @@ pause>nul
 cls&goto main
 
 :schedule2
-::TODO
-echo Under construction
+echo Schedule the pc to restart
+echo.
+set /p tm="Enter the time (24h base time HH:MM) that you want the pc to restart: "
+if errorlevel 1 (
+	color 0c
+	echo Error, the command "%sc%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+echo.
+set /p dt="Enter the date (DD/MM/YYYY) you want this rule to start taking effect (by default it's considered today, type today for default option): "
+if errorlevel 1 (
+	color 0c
+	echo Error, the command "%dt%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+if "%dt%" == "today" set dt=%DATE%
+echo What frequency do you want it to restart?
+echo.
+echo 1) Once
+echo 2) Minute
+echo 3) Hourly
+echo 4) Daily
+echo 5) Weekly
+echo 6) Monthly
+echo.
+set /p frec="Type here: "
+
+if errorlevel 1 (
+	color 0c
+	echo Error, the command "%sc%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if "%frec%" == "1" set frec=ONCE
+if "%frec%" == "2" set frec=MINUTE
+if "%frec%" == "3" set frec=HOURLY
+if "%frec%" == "4" set frec=DAILY
+if "%frec%" == "5" set frec=WEEKLY
+if "%frec%" == "6" set frec=MONTHLY
+
+if errorlevel 1 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if errorlevel 2 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if errorlevel 3 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if errorlevel 4 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if errorlevel 5 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+
+if errorlevel 6 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto schedule2
+)
+echo.
+echo Applying settings...
+echo TASKNAME:PCRESTART > settings.dat
+echo SCHEDULE:%dt% %tm% >> settings.dat
+echo FREQUENCY:%frec% >> settings.dat
+schtasks /create /sc %frec% /tn PCRESTART /tr "shutdown /r" /st %tm% /sd %dt% >nul
+if errorlevel 1 (
+	color 0c
+	echo An error ocurred!
+	echo Restart the program and try again
+	echo.
+	echo        [PRESS ENTER TO QUIT]
+	pause>nul
+	cls&goto finish
+)
+echo.
+echo SUCCESS: The computer has been programmed to restart on %dt% at %tm% successfully
+echo.
+echo         [PRESS ENTER GO TO MENU]
+echo.
+pause>nul
+cls&goto main
+
+:deletesch
+echo Delete a schedule
+echo.
+echo.
+echo Choose what schedule you want to delete:
+echo.
+set ps="PCSHUTDOWN"
+set pr="PCRESTART"
+schtasks /query /tn "PCSHUTDOWN"
+if not errorlevel 1 (
+	echo 1) 
+	schtasks /query /tn "PCSHUTDOWN"
+) else (
+	schtasks /query /tn "PCRESTART"
+	if not errorlevel 1 (
+		echo 1)
+		set ps="PCSHUTDOWN"
+		schtasks /query /tn "PCRESTART"
+	) else (
+		echo No schedules to show
+		echo.
+		echo         [PRESS ENTER GO TO MENU]
+		echo.
+		pause>nul
+		cls&goto main
+	)
+)
+schtasks /query /tn "PCRESTART"
+if not errorlevel 1 (
+	echo 2)
+	schtasks /query /tn "PCRESTART"
+)
+set /p opt="Type here: "
+if "%opt%" == "1" schtasks /delete /tn %ps%
+if "%opt%" == "2" schtasks /delete /tn %pr%
+if errorlevel 1 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto deletesch
+)
+
+if errorlevel 2 (
+	color 0c
+	echo Error, the command "%frec%" wasn't recognized
+	echo.
+	echo        [PRESS ENTER TYPE AGAIN]
+	pause>nul
+	color %themec%
+	cls&goto deletesch
+)
+echo.
+echo         [PRESS ENTER GO TO MENU]
 echo.
 pause>nul
 cls&goto main
